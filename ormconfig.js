244900f5f7c9cd973b3dbf8ff31466c90296ff09
Amauri Oliveira <amaurioliveira@tokenlab.com.br>
2021-02-05 15:49:27 -0300

    Ajuste

--- ormconfig.js
+++ ormconfig.js
@@ -5,9 +5,7 @@ module.exports = {
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'amauri32',
   database: process.env.DB_NAME || 'nest_api',
+  url: process.env.DATABASE_URL,
   entities: ['dist/**/*.entity{.ts,.js}'],
-  url:
-    process.env.DATABASE_URL ||
-    `postgres://${username}:${password}@${host}:${port}/${database}`,
   synchronize: process.env.DB_SYNC == 'true' || false,
 };
